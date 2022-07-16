670c7023055238198289b6a5db6895d05a20811e
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-07-16 12:11:03 +0200

    add login form

---
+++ components/LoginForm.tsx
@@ -0,0 +1,23 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+import Avatar from "./Avatar.tsx";
+
+interface LoginFormProps {
+  password: string;
+}
+
+export default function LoginForm(props: LoginFormProps) {
+  const { password } = props;
+  return (
+    <form>
+      <Avatar />
+      <div className={tw`p-2`} />
+      <input
+        className={tw("block text-center leading-loose bg-[#000] text-white")}
+        type="password"
+        placeholder={`Type "${password}" to enter`}
+      />
+    </form>
+  );
+}
