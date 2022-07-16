6c21385dafa2bd0dab87191f38151cfeea08caf1
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-07-16 12:10:20 +0200

    format

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -2,11 +2,13 @@
 import { h } from "preact";
 import { tw } from "@twind";
 
-
 export default function Avatar() {
   return (
     <div className={tw("w-40 h-40 relative rounded-[50%] overflow-hidden")}>
-      <img className={tw("absolute inset-0 w-full h-full object-cover")} src="/avatar.jpg" />
+      <img
+        className={tw("absolute inset-0 w-full h-full object-cover")}
+        src="/avatar.jpg"
+      />
     </div>
   );
 }
