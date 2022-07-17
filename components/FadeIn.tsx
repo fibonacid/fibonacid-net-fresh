08e35b798d6de905c6b2113256aedc3ed2ae3daf
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-07-17 15:42:40 +0200

    create fade in component

---
+++ components/FadeIn.tsx
@@ -0,0 +1,26 @@
+/** @jsx h */
+import { ComponentChild, h } from "preact";
+import { useLayoutEffect, useRef } from "preact/hooks";
+
+export interface FadeInProps {
+  children: ComponentChild;
+  timeline: GSAPTimeline;
+  vars?: GSAPTimelineVars;
+  position?: gsap.Position;
+}
+
+export default function FadeIn(props: FadeInProps) {
+  const { children, timeline, vars = {}, position } = props;
+  const ref = useRef<HTMLSpanElement>(null);
+
+  useLayoutEffect(() => {
+    if (ref.current) {
+      timeline.from(ref.current.children, {
+        opacity: 0,
+        ...vars,
+      }, position);
+    }
+  }, []);
+
+  return <span ref={ref}>{children}</span>;
+}
