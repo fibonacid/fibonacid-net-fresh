de06ac73f7aa88f5774ea9b8eab4103dd5246b6e
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-07-17 13:14:48 +0200

    make styles extensible

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,10 +1,25 @@
 /** @jsx h */
 import { h } from "preact";
-import { tw } from "@twind";
+import { apply, tw } from "@twind";
 
-export default function Avatar() {
+interface AvatarProps {
+  className?: string;
+}
+
+const avatar = apply`
+  w-40 
+  h-40 
+  relative 
+  rounded-[50%] 
+  overflow-hidden
+  border
+  border-gray
+`;
+
+export default function Avatar(props: AvatarProps) {
+  const { className } = props;
   return (
-    <div className={tw("w-40 h-40 relative rounded-[50%] overflow-hidden")}>
+    <div className={tw(avatar, className)}>
       <img
         className={tw("absolute inset-0 w-full h-full object-cover")}
         src="/avatar.jpg"
