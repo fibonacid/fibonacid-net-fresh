2d976bc356b9546a481a9a5561a7ad8b665aabc1
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-07-17 16:21:27 +0200

    create fade out component

---
+++ components/FadeOut.tsx
@@ -0,0 +1,29 @@
+/** @jsx h */
+import { ComponentChild, h } from "preact";
+import { useEffect, useLayoutEffect, useRef, useState } from "preact/hooks";
+
+export interface FadeOutProps {
+  in: boolean;
+  children: ComponentChild;
+  timeline: GSAPTimeline;
+  vars?: gsap.TweenVars;
+  position?: gsap.Position;
+}
+
+export default function FadeOut(props: FadeOutProps) {
+  const { children, timeline, vars = {}, position } = props;
+  const [active, setActive] = useState(true);
+  const ref = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      timeline.to(ref.current, {
+        opacity: 0,
+        ...vars,
+        onComplete: () => setActive(false),
+      }, position);
+    }
+  }, []);
+
+  return active ? <span ref={ref}>{children}</span> : null;
+}
