9eb82d19e5262e2293c4fc5f4bf0fbcb4b378a6e
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-07-21 21:07:24 +0200

    redirect client side

--- islands/LoginForm.tsx
+++ islands/LoginForm.tsx
@@ -1,7 +1,7 @@
 /** @jsx h */
 import { apply, tw } from "@twind";
 import { h } from "preact";
-import { useState } from "preact/hooks";
+import { useCallback, useState } from "preact/hooks";
 import Avatar from "../components/Avatar.tsx";
 import FadeIn from "../components/FadeIn.tsx";
 import FadeInList from "../components/FadeInList.tsx";
@@ -10,25 +10,29 @@ import { MouseCursor } from "../components/MouseCursor.tsx";
 import Spacer from "../components/Spacer.tsx";
 import gsap from "../utils/gsap.ts";
 
-type LoginFormProps = Pick<FormProps, "onSubmit" | "className">;
-
 const form = apply`
   flex
   flex-col
   items-center
 `;
 
-export default function LoginForm(props: LoginFormProps) {
-  const { className, onSubmit } = props;
+const redirect = (word: string) => {
+  window.location.href = "/api/login?word=" + word;
+};
+
+export default function LoginForm() {
   const [timeline] = useState(() => gsap.timeline());
   const [word, setWord] = useState<string>();
   const [showSubmit, setShowSubmit] = useState(false);
-
   return (
     <Form
-      className={tw(form, className)}
+      className={tw(form)}
       action="/api/login"
-      onSubmit={onSubmit}
+      onSubmit={(event) => {
+        event.preventDefault();
+        const { value } = event.currentTarget;
+        redirect(value)
+      }}
       onBlur={() => {
         setShowSubmit(!!word?.length);
       }}
