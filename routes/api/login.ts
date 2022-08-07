bdbc4436d2f2ae6307a9607b069f3c9882ad68be
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-08-07 17:02:59 +0200

    fix error

--- routes/api/login.ts
+++ routes/api/login.ts
@@ -1,11 +1,13 @@
 import { Handlers } from "$fresh/server.ts";
 
+const siteUrl = Deno.env.get("SITE_URL") || "http://localhost:8000";
+
 export const handler: Handlers = {
   GET(req, _ctx) {
     const url = new URL(req.url);
     const word = url.searchParams.get("word");
     if (word) {
-      return Response.redirect(Deno.env.get("SITE_URL") || "http://localhost:8000/" + word);
+      return Response.redirect(siteUrl + "/" + word);
     }
     return Response.error();
   },
