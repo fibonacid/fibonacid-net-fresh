7b8e8e1d86ed9c0778b7b74dd6fd5f86f142dc47
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-08-22 16:00:48 +0200

    remove joke

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,10 +3,9 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/api/login.ts";
-import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/rhyme.tsx";
+import * as $1 from "./routes/api/login.ts";
+import * as $2 from "./routes/index.tsx";
+import * as $3 from "./routes/rhyme.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/LoginForm.tsx";
 import * as $$2 from "./islands/Rhyme.tsx";
@@ -14,10 +13,9 @@ import * as $$2 from "./islands/Rhyme.tsx";
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/api/login.ts": $2,
-    "./routes/index.tsx": $3,
-    "./routes/rhyme.tsx": $4,
+    "./routes/api/login.ts": $1,
+    "./routes/index.tsx": $2,
+    "./routes/rhyme.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
