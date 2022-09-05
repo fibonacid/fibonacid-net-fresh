6e964ff12332473e0c1e22df02fd86b10c1b170a
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-09-05 15:38:53 +0200

    print env vars

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,14 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+import { IS_BROWSER } from "$fresh/runtime.ts";
 import Layout from "../components/Layout.tsx";
 import Center from "../components/Center.tsx";
 import LoginForm from "../islands/LoginForm.tsx";
 
 export default function Home() {
+  if (!IS_BROWSER) console.log(Deno.env.toObject());
+
   return (
     <Layout>
       <Center className={tw`font-sans bg-black flex`}>
